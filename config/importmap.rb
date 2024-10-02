24dafbdfbec6a7c05a629529593e1717e24181be
Carlos Belda Bustin <carlosbeldabustin@gmail.com>
2024-10-02 09:53:38 +0100

    Add dotenv-rails

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+// preload: true
+// pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+// pin "bootstrap" # @5.3.3
